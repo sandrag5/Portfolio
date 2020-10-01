1425c0b7ec261afe7cf8457f4d2cdf99ea7f27b3
Sandra García-Siñeriz <sandra.garcia.h@gmv.com>
2020-10-01 11:54:04 +0200

    feat(src/app): add Angular routing

--- MinimalistPortolio/src/app/app.module.ts
+++ MinimalistPortolio/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AboutMeModule } from './modules/about-me/about-me.module';
+import { ContactMeModule } from './modules/contact-me/contact-me.module';
+import { LandingViewModule } from './modules/landing-view/landing-view.module';
+import { PortfolioViewModule } from './modules/portfolio-view/portfolio-view.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AboutMeModule,
+    ContactMeModule,
+    LandingViewModule,
+    PortfolioViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
