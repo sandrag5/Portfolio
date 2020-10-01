a4aa4795c8c135b549c1baec444f03ac671c9773
Sandra García-Siñeriz <sandra.garcia.h@gmv.com>
2020-10-01 11:57:01 +0200

    feat(src/app): add host listener on scroll

--- MinimalistPortolio/src/app/app.component.ts
+++ MinimalistPortolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MinimalistPortolio';
+  
+  constructor(
+    private router: Router
+  ) { }
+
+  //Info: https://stackoverflow.com/questions/53998080/how-to-scroll-with-router-navigation-in-angular-7
+  @HostListener('window:wheel', ['$event'])
+  onWheelScroll(evento: WheelEvent) {
+    // Scroll down
+    if (evento.deltaY > 0) {
+      switch (this.router.url) {
+        case '/welcome': {
+          this.router.navigate(['/about'])
+          break
+        }
+        case '/about': {
+          this.router.navigate(['/contact'])
+          break
+        }
+        case '/contact': {
+          break
+        }
+      }
+    } else { 
+      // Scroll up
+      switch (this.router.url) {
+        case '/welcome': {
+          break
+        }
+        case '/about': {
+          this.router.navigate(['/welcome'])
+          break
+        }
+        case '/contact': {
+          this.router.navigate(['/about'])
+          break
+        }
+      }
+    }
+  }
 }
